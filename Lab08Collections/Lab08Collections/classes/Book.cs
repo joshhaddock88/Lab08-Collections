c75c7fb0cfd2f2de72919e8b2053a20c647ec528
Joshua Haddock <joshhaddock88@gmail.com>
2021-07-21 23:16:15 -0700

    Initial commit with updated README and some features

---
+++ Lab08Collections/Lab08Collections/classes/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08Collections.classes
+{
+  public class Book
+  {
+    public string Title { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int BookLength { get; set; }
+
+    public Book()
+    {
+      Console.Write("Title: ");
+      Title = Console.ReadLine();
+      Console.Write("Author first name: ");
+      FirstName = Console.ReadLine();
+      Console.Write("Author last name: ");
+      LastName = Console.ReadLine();
+      Console.Write("Number of pages: ");
+      BookLength = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
